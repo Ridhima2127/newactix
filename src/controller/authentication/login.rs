027752a30013825a56014609c68f88961756572a
Ridhima Khyalappa <ridhima_khyalappa@heartcore.co.jp>
2023-09-14 17:03:23 +0530

    password

--- src/controller/authentication/login.rs
+++ src/controller/authentication/login.rs
@@ -8,6 +8,7 @@ use liquid::{Object, ObjectView};
 use serde::Deserialize;
 use serde::Serialize;
 use std::fs;
+use thiserror::Error;
 
 #[derive(Deserialize, Clone)]
 pub struct User {
@@ -15,6 +16,19 @@ pub struct User {
     pub(crate) password: String,
 }
 
+#[derive(thiserror::Error, Debug)]
+pub enum AuthError {
+    #[error("Invalid credentials.")]
+    InvalidCredentials(#[source] anyhow::Error),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+/*pub struct Credentials {
+    pub username: String,
+    pub password: Secret<String>,
+}
+*/
 pub async fn get_user_by_username(username: &str) -> Option<User> {
     match get_users().await {
         Ok(users) => users.iter().find(|user| user.username == username).cloned(),
@@ -71,3 +85,33 @@ pub async fn login() -> Result<HttpResponse> {
         .content_type("text/html; charset=utf-8")
         .body(output))
 }
+
+/*#[tracing::instrument(name = "Validate credentials", skip(credentials, pool))]
+pub async fn validate_credentials(
+    credentials: Credentials,
+    pool: &PgPool,
+) -> Result<uuid::Uuid, AuthError> {
+    let mut user_id = None;
+    let mut expected_password_hash = Secret::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$\
+        gZiV/M1gPc22ElAH/Jh1Hw$\
+        CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
+            .to_string(),
+    );
+    if let Some((stored_user_id, stored_password_hash)) =
+        get_stored_credentials(&credentials.username, pool).await?
+    {
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    }
+
+    spawn_blocking_with_tracing(move || {
+        verify_password_hash(expected_password_hash, credentials.password)
+    })
+        .await
+        .context("Failed to spawn blocking task.")??;
+
+    user_id
+        .ok_or_else(|| anyhow::anyhow!("Unknown username."))
+        .map_err(AuthError::InvalidCredentials)
+}*/
